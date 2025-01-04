767813dc627d3e538ed5bad9ea465f7b6ccbbff4
Gihed Annabi <annabi.gihed@gmail.com>
2025-01-04 19:40:05 +0100

    precising type of errors

--- Src/Tools/Templates.Core.Api/Conventions/ApiConventions.cs
+++ Src/Tools/Templates.Core.Api/Conventions/ApiConventions.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Http;
 namespace Templates.Core.Tools.API.Conventions;
 public static class ApiConventions
 {
-	[ProducesResponseType(StatusCodes.Status200OK)]
-	[ProducesResponseType(StatusCodes.Status404NotFound)]
-	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProblemDetails))]
+	[ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+	[ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
 	public static void Default<T>() { }
 
-	[ProducesResponseType(StatusCodes.Status200OK)]
-	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProblemDetails))]
+	[ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
 	public static void DefaultNoNotFound<T>() { }
 }
