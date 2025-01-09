f937f432a0ac7dfdbec90733653c77bd715fe08a
Gihed Annabi <annabi.gihed@gmail.com>
2025-01-09 13:01:18 +0100

    correction

--- Src/Infrastructure/Templates.Core.Infrastructure.Messaging.EntityFrameworkCore/Outbox/Services/OutboxPublisherService.cs
+++ Src/Infrastructure/Templates.Core.Infrastructure.Messaging.EntityFrameworkCore/Outbox/Services/OutboxPublisherService.cs
@@ -1,22 +1,18 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Templates.Core.Infrastructure.Persistence.EntityFrameworkCore.Outbox.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 using Templates.Core.Infrastructure.Messaging.EntityFrameworkCore.RabbitMQ.MessagePublisher;
+using Templates.Core.Infrastructure.Persistence.EntityFrameworkCore.Outbox.Repositories;
 
 namespace Templates.Core.Infrastructure.Messaging.EntityFrameworkCore.Outbox.Services;
 
 public class OutboxPublisherService : BackgroundService
 {
-
-	protected readonly IOutboxRepository _outboxRepository;
-	protected readonly IMessagePublisher _messagePublisher;
+	private readonly IServiceProvider _serviceProvider;
 	protected readonly ILogger<OutboxPublisherService> _logger;
 
-	public OutboxPublisherService(IOutboxRepository outboxRepository, IMessagePublisher messagePublisher,
-								   ILogger<OutboxPublisherService> logger)
+	public OutboxPublisherService(IServiceProvider serviceProvider, ILogger<OutboxPublisherService> logger)
 	{
-		_outboxRepository = outboxRepository;
-		_messagePublisher = messagePublisher;
 		_logger = logger;
 	}
 
@@ -24,19 +20,27 @@ public class OutboxPublisherService : BackgroundService
 	{
 		while (!stoppingToken.IsCancellationRequested)
 		{
-			var messages = await _outboxRepository.GetUnprocessedMessagesAsync(stoppingToken);
-
-			foreach (var message in messages)
+			using (var scope = _serviceProvider.CreateScope())
 			{
-				try
+				var outboxRepository = scope.ServiceProvider.GetRequiredService<IOutboxRepository>();
+				var messagePublisher = scope.ServiceProvider.GetRequiredService<IMessagePublisher>();
+
+				var messages = await outboxRepository.GetUnprocessedMessagesAsync(stoppingToken);
+
+				foreach (var message in messages)
 				{
-					await _messagePublisher.PublishAsync(message.Payload);
-					await _outboxRepository.MarkAsProcessedAsync(message.Id, stoppingToken);
-					_logger.LogInformation($"Message {message.Id} published successfully.");
-				}
-				catch (Exception ex)
-				{
-					_logger.LogError(ex, $"Error publishing message {message.Id}");
+					try
+					{
+						await messagePublisher.PublishAsync(message.Payload);
+						await outboxRepository.MarkAsProcessedAsync(message.Id, stoppingToken);
+						_logger.LogInformation($"Message {message.Id} published successfully.");
+					}
+					catch (Exception ex)
+					{
+						_logger.LogError(ex, $"Error publishing message {message.Id}");
+					}
+					// Add a delay to avoid tight loops
+					await Task.Delay(1000, stoppingToken);
 				}
 			}
 		}
